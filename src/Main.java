e89862dc7bd8982a24c87d428daa0cd00b312925
JUAN OSCAR CAHUICH EUAN <13470522@itcampeche.edu.mx>
2016-04-12 23:34:32 -0500

    CuentaBancaria, CuentaDeAhorro y CuentaDeCheques

--- src/Main.java
+++ src/Main.java
@@ -44,44 +44,37 @@ public class Main {
          * System.out.println("saldo: " + misAhorros.getsaldo());
          */
 
-        System.out.println("¿Que cuenta desea abrir?");
-        System.out.println("[1] = Bancaria [2]= Ahorro");
+        System.out.println("¡Elige la opción: *.* *_*!");
+        System.out.println("[1] Bancaria [2] Ahorro [3] Cheques");
         int cuenta = leer.nextInt();
-        
+        System.out.println("--------------------------------------------------");
         if (cuenta == 1) {
             CuentaBancaria cuenta1 = new CuentaBancaria(100154325, "juan oscar");
             int contador = 0;
-            System.out.println(cuenta1.getnumeroDeCuenta() + " " + cuenta1.getcliente());
+            System.out.println("NUMERO DE CUENTA: " + cuenta1.getnumeroDeCuenta() + " " + "NOMBRE: " + cuenta1.getcliente());
             while (contador == 0) {
-                System.out.println("¿Que accion desea realizar?");
-                System.out.println("1 = Depositar");
-                System.out.println("2 = Retirar");
-                System.out.println("3 = Finalizar");
-                int accion = leer.nextInt();
-                if (accion == 1) {
-
-                    System.out.println("¿Cuanto dinero deseas depositar?");
-                    double cantidad = leer.nextDouble();
-                    cuenta1.Depositar(cantidad);
+              System.out.println(" ¿Que accion desea realizar?\n 1 = Depositar \n 2 = Retirar \n 3 = Finalizar \n");
+                int a = leer.nextInt();
+                if (a == 1) {
+                  System.out.println("¿Cuanto dinero deseas depositar?");
+                    cuenta1.Depositar(leer.nextDouble());
                 }
-                if (accion == 2) {
+                if (a == 2) {
                     System.out.println("¿Cuanto dinero deseas retirar?");
-                    double cantidad = leer.nextDouble();
-                    cuenta1.Retirar(cantidad);
+                     cuenta1.Retirar(leer.nextDouble());
                 }
-                if (accion == 3) {
+                if (a == 3) {
                     System.out.println("Fin");
                     contador = 1;
                 }
-                double saldo = cuenta1.getSaldo();
-                System.out.println(+saldo);
+              System.out.println("saldo: " + cuenta1.getSaldo());
             }
         }
-
+  
         if (cuenta == 2) {
-            CuentaDeAhorro cuenta2
-                    = new CuentaDeAhorro(100154325, "juan oscar");
-            System.out.println(cuenta2.getnumeroDeCuenta() + " " + cuenta2.getcliente());
+
+            CuentaDeAhorro cuenta2 = new CuentaDeAhorro(100154325, "juan oscar");
+            System.out.println("NUMERO DE CUENTA: " + cuenta2.getnumeroDeCuenta() + " " + "NOMBRE: " + cuenta2.getcliente());
             System.out.println(cuenta2.getSaldo());
             cuenta2.Depositar(200);
             System.out.println(cuenta2.getSaldo());
@@ -90,8 +83,30 @@ public class Main {
             cuenta2.Retirar(500);
             System.out.println(cuenta2.getSaldo());
             cuenta2.corteMensual();
-            System.out.println(cuenta2.getSaldo());
-
+            System.out.println("saldo: " + cuenta2.getSaldo());
+        }
+     
+        if (cuenta == 3) {
+            CuentaDeCheques cuenta3 = new CuentaDeCheques(100154325, "juan oscar");
+            System.out.println("NUMERO DE CUENTA: " + cuenta3.getnumeroDeCuenta() + " " + "NOMBRE: " + cuenta3.getcliente());
+            int contador = 0;
+            while (contador == 0) {
+            System.out.println(" ¿Que accion desea realizar?\n 1 = Depositar \n 2 = Retirar \n 3 = Finalizar \n");
+                int x = leer.nextInt();
+                if (x == 1) {
+                    System.out.println("¿Cuanto deseas depositar?");
+                     cuenta3.Depositar(leer.nextDouble());
+                }
+                if (x == 2) {
+                    System.out.println("¿Cuanto deseas retirar?");
+                    cuenta3.Retirar(leer.nextDouble());
+                }
+                if (x == 3) {
+                    System.out.println("Fin");
+                    contador = 1;
+                }
+              System.out.println("saldo: " +cuenta3.getSaldo());
+            }
         }
     }
-}
+    }
